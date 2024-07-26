7e3f00af3db52c1a7d0624254200f85749b8195e
fabiCoseglia <fabiancoseglia@gmail.com>
2024-07-25 21:11:00 -0300

    finish cart

--- src/components/CartModal.jsx
+++ src/components/CartModal.jsx
@@ -1,5 +1,10 @@
+import { useContext } from "react"
+import { CartContext } from "../context/cartContext"
 
+// eslint-disable-next-line react/prop-types
 export const CartModal = ({handleToggle}) => {
+  const {clearCart,cart,increaseItemQuantity,decreaseItemQuantity,removeItemFromCart} = useContext(CartContext);
+
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
     <div className="bg-white p-8 rounded-lg shadow-lg w-11/12 max-w-md">
@@ -10,30 +15,36 @@ export const CartModal = ({handleToggle}) => {
         </button>
       </div>
       <ul className="divide-y divide-gray-200">
-        <li className="py-4 flex justify-between items-center">
+        {
+          cart.length !== 0 ?
+          cart.map((product,i)=>(
+            <li className="py-4 flex justify-between items-center" key={i}>
           <div className="flex items-center">
-            <img src="https://via.placeholder.com/50" alt="Producto" className="h-12 w-12 rounded object-cover" />
+            <img src={product.image} alt="Producto" className="h-12 w-12 rounded object-cover" />
             <div className="ml-4">
-              <p className="text-sm font-medium">Nombre del Producto</p>
-              <p className="text-sm text-gray-500">$20.00</p>
+              <button className="text-sm font-medium"> {product.title} </button>
+              <p className="text-sm text-gray-500">${product.price} </p>
             </div>
           </div>
           <div className="flex items-center">
-            <button className="text-gray-600 hover:text-gray-800 px-2">-</button>
-            <span className="px-2">1</span>
-            <button className="text-gray-600 hover:text-gray-800 px-2">+</button>
-            <button className="ml-4 text-red-500 hover:text-red-700">Eliminar</button>
+            <button className="w-4 bg-red-500 hover:bg-red-600 text-white rounded-md" onClick={()=>decreaseItemQuantity(product.id)}>-</button>
+            <span className="px-2"> {product.quantity} </span>
+            <button className=" w-4  bg-green-500 hover:bg-green-600 text-white rounded-md" onClick={()=>increaseItemQuantity(product.id)}>+</button>
+            <button className="ml-4 text-red-500 hover:text-red-700" onClick={()=>removeItemFromCart(product.id)}>Delete</button>
           </div>
         </li>
-        {/* Repite este bloque para más productos */}
+          ))
+          : <p className="text-gray-400 text-center">Empty cart</p>
+        }
       </ul>
+      
       <div className="mt-4">
         <div className="flex justify-between items-center font-medium">
           <span>Total:</span>
-          <span>$40.00</span>
+          <span className="font-bold">${cart.reduce((total, item) => total + item.price * item.quantity, 0).toFixed(2)}</span>
         </div>
-        <button className="mt-4 w-full bg-violet-500 hover:bg-violet-600 text-white py-2 rounded-lg">Ir a pagar</button>
-        <button className="mt-2 w-full bg-red-500 hover:bg-red-600 text-white py-2 rounded-lg">Vaciar carrito</button>
+        <button className="mt-4 w-full bg-violet-500 hover:bg-violet-600 text-white py-2 rounded-lg">Checkout</button>
+        <button className="mt-2 w-full bg-red-500 hover:bg-red-600 text-white py-2 rounded-lg" onClick={clearCart}>Clear cart</button>
       </div>
     </div>
   </div>
--- src/components/IndexProducts.jsx
+++ src/components/IndexProducts.jsx
@@ -1,11 +1,11 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { useLoaderData } from "react-router-dom"
 import { CartContext } from "../context/cartContext";
 import Swal from "sweetalert2";
 
 export const IndexProducts = () => {
     const {products} = useLoaderData();
-    const {addItemToCart} = useContext(CartContext);
+    const {addItemToCart,cart} = useContext(CartContext);
     
     const handleAddItemToCart = (product)=>{
       const Toast = Swal.mixin({
@@ -20,25 +20,29 @@ export const IndexProducts = () => {
         icon: 'success',
         title: 'product added to cart',
       });
-      console.log(product);
       addItemToCart(product)
-    }
+    };
+
+    useEffect(() => {
+      console.log(cart); // Esto se ejecutará después de que el estado del carrito se actualice
+    }, [cart]);
   return (
     <div className="container mx-auto p-12">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-        {products.map((product) => (
-          <div key={product.id} className="bg-white shadow-md rounded-lg overflow-hidden flex flex-col">
-            <img src={product.image} alt={product.title} className="w-full h-48 object-contain p-3" />
-            <div className="p-4 flex flex-col flex-1">
-              <h2 className="text-xl font-bold mb-2">{product.title}</h2>
-              <p className="text-green-700 mb-4 font-bold">${product.price.toFixed(2)}</p>
-              <button className="mt-auto w-full bg-violet-500 hover:bg-violet-600 text-white py-2 rounded-lg" onClick={()=>handleAddItemToCart(product)}>
-                Agregar al carrito
-              </button>
-            </div>
+  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+    {products.map((product) => (
+      <div key={product.id} className="bg-white shadow-md rounded-lg overflow-hidden flex flex-col">
+        <img src={product.image} alt={product.title} className="w-full h-48 object-contain p-3" />
+        <div className="p-4 flex flex-col justify-between items-start flex-1">  <h2 className="text-xl font-bold mb-2">{product.title}</h2>
+          <div>
+            <p className="text-green-700 mb-4 font-bold">${product.price.toFixed(2)}</p>
+            <button className="mt-auto p-2 w-full bg-violet-500 hover:bg-violet-600 text-white py-2 rounded-lg" onClick={()=>handleAddItemToCart(product)}>
+              Add to cart
+            </button>
           </div>
-        ))}
+        </div>
       </div>
-    </div>
+    ))}
+  </div>
+</div>
   )
 }
